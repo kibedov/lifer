04381570be03bd8203160573bffeaf632664c4ea
Кибедов Руслан <rkibedov@lanit.ru>
2021-10-28 01:03:08 +0300

    carcass commit

---
+++ src/main/java/ru/kibedov/lifer/LiferApplication.java
@@ -0,0 +1,13 @@
+package ru.kibedov.lifer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kibedov/lifer/LiferApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.kibedov.lifer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class LiferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
